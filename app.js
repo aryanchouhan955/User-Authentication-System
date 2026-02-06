10d7be1b65ce375e77f75e8fb4eeb6a91b3f089b
Aryan Chouhan <aryanchouhan955@gmail.com>
2026-02-06 16:24:48 +0530

    added features: 1. To see profile 2. Edit Prfile 3. Delete Profile

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.json());
 // database
 const userData = require('./userModel');
 
+// home page
 app.get('/', (req, res) => {
     res.render('home');
 });
@@ -17,25 +18,69 @@ app.get('/home', (req, res) => {
     res.render('home');
 });
 
+// user want to get login page
 app.get('/login', (req, res) => {
     res.render('userLogin');
 });
 
+// user post login from
 app.post('/login', async (req, res)=>{
     const { userid, password } = req.body;
     const user = await userData.findOne({ userid, password });
     if(user){
-        res.redirect('/logged-in');
+        res.redirect(`/profile/${userid}`);
+    } 
+    else{
+        res.render('userLogin', { msg: 'Please enter correct id and password' });
+    }
+    
+})
+
+// show profile
+app.get('/profile/:userid', async (req, res)=>{
+    const userid = req.params.userid;
+    const user = await userData.findOne({userid});
+    if(user){
+        res.render('userProfile', {userid: user.userid, name: user.name, email: user.email});
     }
     else{
-        res.redirect('/');
+        res.redirect('userLogin', { msg: 'Please enter correct id and password' })
     }
 })
 
-app.get('/logged-in', (req, res)=>{
-    res.send('Logged in succesfully')
+// delete profile
+app.get('/delete/:userid', async(req, res)=>{
+    const userid = req.params.userid;
+    const user = await userData.findOneAndDelete({userid});
+    if(user){
+        res.render('userLogin', {msg: 'Profile Deleted Successfully'});
+    }
+    else{
+        res.render('userLogin', { msg: 'Please enter correct id and password' });
+    }
 })
 
+// edit profile
+app.get('/edit/:userid', async (req, res)=>{
+    const userid = req.params.userid;
+    const user = await userData.findOne({userid});
+    if(user){
+        res.render('edit', {userid, name: user.name, email: user.email, password: user.password});
+    }
+    else{
+        res.render('userLogin', { msg: `Cannot Edit user: ${userid}` });
+    }
+    
+})
+
+app.post('/edit/:userid', async (req, res)=>{
+    const userID = req.params.userid;
+    const {userid, name, email, password} = req.body;
+    await userData.findOneAndUpdate({userid: userID}, {userid, name, email, password});
+    res.redirect(`/profile/${userid}`)
+})
+
+// create user 
 app.get('/create-user', (req, res) => {
     res.render('createUser');
 });
@@ -48,7 +93,7 @@ app.post('/create-user', async (req, res)=>{
         password,
         email
     });
-    res.send(user);
+    res.redirect('/login');
 })
 
 
